850f0e941a7c84dc10536d517888e4b8c1a54be8
axCaffiend <axfulcher@outlook.com>
2025-08-26 01:31:21 +0100

    Added load_inline_svg function to template-functions.php

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -35,3 +35,36 @@ function lambda_theme_pingback_header() {
 	}
 }
 add_action( 'wp_head', 'lambda_theme_pingback_header' );
+
+
+
+/* ----- CUSTOM FUNCTIONS -----*/
+
+/* --- Load inline SVG --- */
+/* Returns contents of inline SVG and inserts into DOM
+- Solution: https://enshrined.co.uk/2018/09/19/how-to-properly-include-inline-svgs-in-a-wordpress-theme/
+
+!! NOTE: Uses unsecure solution since switches off SSL certificate verification.
+	Didn't work:
+	- Tried downloading different certificate bundle
+	- Tried adding openssl.cafile = path/to/git/openssl/cert.pem to php.ini file
+	- Could be to do with Local, PHP version, finding right certificate bundle, or not working over HTTPS??
+
+	- Solution (from first answer): https://stackoverflow.com/questions/26148701/file-get-contents-ssl-operation-failed-with-code-1-failed-to-enable-crypto
+
+*/
+function load_inline_svg($svg_filepath) {
+
+	$arrContextOptions= [
+		'ssl' => [
+			// 'cafile' => '/wp-includes/certificates/curl-ca-bundle.crt',
+			'verify_peer'=> false,
+			'verify_peer_name'=> false,
+		],
+	];
+
+	if (file_exists(get_stylesheet_directory() . $svg_filepath)) {
+		return file_get_contents(get_stylesheet_directory_uri() . $svg_filepath, true, stream_context_create($arrContextOptions));
+	}
+	return '';
+}
